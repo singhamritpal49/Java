48d4ba6323808901a8c129e4b7802df739145664
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-03 01:33:13 -0400

    String

---
+++ Strings/src/com/amritpalsingh/Main.java
@@ -0,0 +1,19 @@
+package com.amritpalsingh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+        // String
+        String myString ="This is my String";
+        myString = myString + "Added more worlds";
+        System.out.println(myString);
+    }
+}
