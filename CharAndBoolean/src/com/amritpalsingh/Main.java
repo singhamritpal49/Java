9106e1358a16ca571139f3c73ad194c12cc67f6d
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-03 01:11:32 -0400

    boolean and char

---
+++ CharAndBoolean/src/com/amritpalsingh/Main.java
@@ -0,0 +1,17 @@
+package com.amritpalsingh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u00A9';
+        // char allows you to save One character;
+//        System.out.println(myChar);
+//        System.out.println(myUnicodeChar);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+        boolean isCustomerOver21 = false;
+    }
+
+}
