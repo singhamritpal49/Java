175ff4f7d51a9ddd221719982bc572df0f9ed138
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-04 16:01:17 -0400

    arrays and operators

--- Operators/src/com/amritpalsingh/Main.java
+++ Operators/src/com/amritpalsingh/Main.java
@@ -7,11 +7,34 @@ public class Main {
         int result = 1 + 2; // 1 + 2 = 3;
         System.out.println(result);
         int previousResult = result;
-        System.out.println("PreviousResults = " + previousResult);
+//        System.out.println("PreviousResults = " + previousResult);
         result = result - 1;
         result  = result * 10;
 
         result = result / 5;
-        System.out.println(result);
+//        System.out.println(result);
+
+        // if then
+        boolean isAlien = false;
+        if (isAlien) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("YOYO");
+        }
+
+        int topScore = 100;
+        int lowScore = 1;
+        if (topScore == 10 || lowScore == 1) {
+            System.out.println("Your got the highest Score");
+        }
+
+        // Challenge
+        double firstValue = 20.00;
+        double secondValue = 80.00;
+        double totalValue = firstValue + secondValue * 100.00d;
+        double remainderValue = totalValue % 40;
+
+        }
+
     }
-}
+
