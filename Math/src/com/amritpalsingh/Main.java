5abfa783b55f7d6718524f61281cf0d4292bf6ec
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-04 19:16:24 -0400

    Math class

---
+++ Math/src/com/amritpalsingh/Main.java
@@ -0,0 +1,22 @@
+package com.amritpalsingh;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Rounding number
+//        int result = Math.round(1.1f);
+
+//        int result = (int)Math.ceil(1.1F);
+
+//        int result = (int)Math.floor(1.1);
+
+//          int result = Math.max(1,2);
+//          int result = Math.min(1,2);
+
+//        int result =  (int)Math.round(Math.random() * 100);
+        int result =  (int) (Math.random() * 100);
+        System.out.println(result);
+
+    }
+}
