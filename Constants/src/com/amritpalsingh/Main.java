71a1d90b78de5bc894ff0bef37356a19ee3c147b
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-04 16:05:46 -0400

    constants

---
+++ Constants/src/com/amritpalsingh/Main.java
@@ -0,0 +1,12 @@
+package com.amritpalsingh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // add final to make it constant
+
+        final float pi = 3.14f;
+        // cannot do this pi = 1;
+
+    }
+}
