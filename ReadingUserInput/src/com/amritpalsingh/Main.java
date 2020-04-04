31bec7db6895fc80d80fbfaa531476ce4e620bcc
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-04 19:45:19 -0400

    Getting User Input With Scanner

---
+++ ReadingUserInput/src/com/amritpalsingh/Main.java
@@ -0,0 +1,16 @@
+package com.amritpalsingh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Getting User Input from user Use Scanner System.in
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" Enter Your Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Your age " + name);
+
+
+    }
+}
