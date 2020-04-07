460fe1784c5fc4f51173c8efa0613d82fdf6ed27
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-06 22:22:07 -0400

    refactored

--- MortgageCalculator/src/com/amritpalsingh/Main.java
+++ MortgageCalculator/src/com/amritpalsingh/Main.java
@@ -11,20 +11,25 @@ public class Main {
        int principal =  (int) readNumber("Principal ($1K - $1M): ",1000,1_000_000);
       float annualInterest = (float) readNumber("Annual Interest Rate: ",1,30);
         byte years = (byte) readNumber("Period (Years): ",1,30);
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
 
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculateMortgage(principal,annualInterest,years);
         String mortgageFormat = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payment: " + mortgageFormat);
+    }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println("PAYMENT SCHEDULED");
         System.out.println("-----------------");
         for(short month = 1; month <= years * MONTH_IN_YEAR;month++) {
             double balance = calculateBalance(principal,annualInterest,years,month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
-
     }
 
     public static double readNumber(String prompt,double min, double max) {
